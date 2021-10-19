d4b172de67c7bf0cce9b3d2b8ac0e8e51bd9341b
ufatfat <lzff@ufatfat.com>
2021-10-20 03:22:57 +0800

    fix:修改baseurl

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,8 +2,7 @@ import axios from "axios"
 // import Vue from "vue"
 
 let instance = axios.create({
-    baseURL: "http://contest.ivillages.org/",
-    // baseURL: "/",
+    baseURL: "/",
     timeout: 600000,
     headers: {
         "Content-Type": "application/json;charset=UTF-8",
